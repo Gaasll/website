e8791f6d57e40d6ab965ab0626b9b6234de5029b
Garabet Aslo <98517170+Gaasll@users.noreply.github.com>
2024-08-14 12:36:40 +0200

    Update AboutPage.vue

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -3,74 +3,151 @@
     <div class="intro-section">
       <h1>My name is Garabet Aslo, but you can call me Garo.</h1>
       <p>
-        I am a social person who likes to take initiatives. I have a big passion for traveling, but usually, all I do is spend my time staring at a computer screen.
+        I am a social person who likes to take initiatives. I have a big passion for traveling, but usually, all I do is
+        spend my time staring at a computer screen.
       </p>
       <p>
-        In the projects section, you will find my experience in Computer Science - and it is forever growing! Below each project, I have links to all of my research and code.
+        In the <a href="https://garabet.netlify.app/work" style="color: white;">projects section</a>, you will
+        find my
+        experience in Computer Science
+        - and it is forever growing! Below each
+        project, I have links to all of my research and code.
       </p>
     </div>
-    <div class="details-section">
-      <h3>Education:</h3>
-      <p>
-        Bachelor of Science in Computer Engineering from KTH, where I studied a wide range of subjects including processors, programming, computer networks, databases, and security. My education also included a focus on cloud services and IoT, enhanced by diverse coursework such as:
-      </p>
-      <ul>
-        <li>Engineering Skills</li>
-        <li>Digital Design</li>
-        <li>Computer Hardware Engineering</li>
-        <li>Algorithms & Data Structures</li>
-        <li>Network & Communication</li>
-        <li>Networking Architectures</li>
-      </ul>
-      <h3>Skills:</h3>
-      <ul>
-        <li>Programming: C, Java, JavaScript, Node.js, HTML/CSS</li>
-        <li>Data Analysis and Algorithms</li>
-        <li>DevOps and SQL</li>
-        <li>Functional Programming </li>
-        <li>Scrum & Project Methods </li>
-        <li>UI/UX Design and Communication</li>
-      </ul>
-      <h3>Professional Experience:</h3>
-      <div class="experience">
-        <strong>Shift Leader at ICA Maxi Stormarknad Lindhagen, Stockholm (August 2019 - Present)</strong>
-        <p>
-          In this role, I lead a team of 8-10 staff, ensuring efficient store operations and delivering exceptional customer service. My responsibilities include personnel management, scheduling, and training, as well as maintaining inventory control and store standards.
-        </p>
+
+    <div class="cards-container">
+      <div class="card" @click="toggleDetails('education')">
+        <h3>Education</h3>
+        <p>KTH Royal Institute of Technology</p>
+        <div class="icon-container">
+          <img src="../assets/education.jpg" alt="Education Icon" />
+
+        </div>
       </div>
-      <div class="experience">
-        <strong>Case Officer at Försäkringskassan, Stockholm (September 2019 - December 2021)</strong>
-        <p>
-          I managed and processed a diverse range of cases, providing comprehensive assistance to clients and collaborating with various departments and external partners. This role honed my communication and problem-solving skills, contributing to a responsible and efficient work environment.
-        </p>
+      <div v-if="showDetails.education" class="details">
+        <p>Bachelor of Science in Computer Engineering</p>
+        <ul>
+          <li>Engineering Skills</li>
+          <li>Digital Design</li>
+          <li>Computer Hardware Engineering</li>
+          <li>Algorithms & Data Structures</li>
+          <li>Network & Communication</li>
+          <li>Networking Architectures</li>
+          <li>Data Storage</li>
+        </ul>
       </div>
-      <div class="experience">
-        <strong>Bachelor Thesis at Northab (March 2024 - June 2024)</strong>
-        <p>
-          For my thesis, I investigated the cost-effectiveness of Infrastructure as a Service (IaaS) and Platform as a Service (PaaS) by analyzing various factors and conducting a case study deploying a recruiting system on multiple cloud platforms. My findings offer insights into optimizing cloud deployment strategies for businesses.
-        </p>
+
+      <div class="card" @click="toggleDetails('skills')">
+        <h3>Skills</h3>
+        <p>Programming, DevOps, UI/UX, and more...</p>
+        <div class="icon-container">
+          <img src="../assets/skills.jpg" alt="Skills Icon" />
+
+        </div>
       </div>
-      <h3>Certifications:</h3>
-      <p>
-        Certificate of Global Competence from KTH Royal Institute of Technology, recognized for proficiency in navigating diverse cultural environments and international settings.
-      </p>
-      <h3>Languages:</h3>
-      <ul>
-        <li>English</li>
-        <li>Swedish</li>
-        <li>Armenian</li>
-      </ul>
-      <h3>Goals:</h3>
-      <p>
-        I am eager to join a dynamic team where I can apply my technical and professional skills to develop innovative solutions and drive progress in the field of technology. Committed to continuous learning, I aspire to contribute to projects that advance digital transformation.
-      </p>
-      <p>
-        Feel free to explore my <a href="#">LinkedIn</a> and <a href="#">Portfolio</a> for more information. Thank you for visiting my page, and I look forward to connecting with you.
-      </p>
+      <div v-if="showDetails.skills" class="details">
+        <ul>
+          <li>Programming: C, Java, JavaScript, Node.js</li>
+          <li>DevOps, AWS, Azure, GCP</li>
+          <li>SQL</li>
+          <li>UI/UX Design and Communication</li>
+          <li>Functional Programming </li>
+          <li>Scrum & Project Methods </li>
+        </ul>
+      </div>
+
+      <div class="card" @click="toggleDetails('experience')">
+        <h3>Experience</h3>
+        <p>Shift Leader, Case Officer, Thesis...</p>
+        <div class="icon-container">
+          <img src="../assets/work.jpg" alt="Experience Icon" />
+
+        </div>
+      </div>
+      <div v-if="showDetails.experience" class="details">
+        <div class="experience">
+          <strong>Shift Leader at ICA Maxi Lindhagen, Stockholm</strong>
+          <p> (August 2019 - Present)</p>
+        </div>
+        <div class="experience">
+          <strong>Case Officer at Försäkringskassan, Stockholm</strong>
+          <p>(September 2019 - December 2021)</p>
+        </div>
+        <div class="experience">
+          <strong>Bachelor Thesis at Northab</strong>
+          <p>(March 2024 - June 2024)</p>
+        </div>
+      </div>
+
+      <div class="card" @click="toggleDetails('certifications')">
+        <h3>Certifications</h3>
+        <p>Global Competence Certificate</p>
+        <div class="icon-container">
+          <img src="../assets/cert.jpg" alt="Certifications Icon" />
+
+        </div>
+      </div>
+      <div v-if="showDetails.certifications" class="details">
+        <p>Certificate of Global Competence from KTH</p>
+      </div>
+
+      <div class="card" @click="toggleDetails('languages')">
+        <h3>Languages</h3>
+        <p>English, Swedish, Armenian</p>
+        <div class="icon-container">
+          <img src="../assets/languages.png" alt="Languages Icon" />
+
+        </div>
+      </div>
+      <div v-if="showDetails.languages" class="details">
+        <ul>
+          <li>English - Fluent</li>
+          <li>Swedish - Fluent</li>
+          <li>Armenian - Fluent</li>
+        </ul>
+      </div>
+
+      <div class="card" @click="toggleDetails('goals')">
+        <h3>Goals</h3>
+        <p>Continuous learning and innovation...</p>
+        <div class="icon-container">
+          <img src="../assets/goal.jpg" alt="Goals Icon" />
+
+        </div>
+      </div>
+      <div v-if="showDetails.goals" class="details">
+        <p>I'm eager to join a dynamic team, apply my skills to drive innovation, and contribute to digital
+          transformation projects.</p>
+        <p>Explore my <a href="https://www.linkedin.com/in/garabetaslo/">LinkedIn</a> and <a
+            href="https://github.com/Gaasll">Github</a>.</p>
+      </div>
     </div>
-    <img src="../assets/code.svg" alt="Coding illustration" class="about-img" />
+
+
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showDetails: {
+        education: false,
+        skills: false,
+        experience: false,
+        certifications: false,
+        languages: false,
+        goals: false
+      }
+    };
+  },
+  methods: {
+    toggleDetails(section) {
+      this.showDetails[section] = !this.showDetails[section];
+    }
+  }
+};
+</script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Urbanist&display=swap');
@@ -83,7 +160,6 @@
   padding: 20px;
   background-color: #f9f9f9;
   color: #333;
-  line-height: 1.6;
 }
 
 .intro-section {
@@ -92,6 +168,7 @@
   padding: 20px;
   border-radius: 8px;
   margin-bottom: 20px;
+  text-align: center;
 }
 
 h1 {
@@ -104,36 +181,44 @@ p {
   margin-bottom: 15px;
 }
 
-h3 {
-  font-size: 24px;
-  margin-top: 20px;
-  margin-bottom: 10px;
-  color: #2980b9;
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 }
 
-ul {
-  font-size: 18px;
-  margin-left: 20px;
-  list-style: disc;
+.card {
+  background-color: #fff;
+  padding: 20px;
+  margin: 10px;
+  border-radius: 8px;
+  text-align: center;
+  width: 300px;
+  cursor: pointer;
+  transition: transform 0.3s ease;
 }
 
-.experience {
-  margin-bottom: 20px;
+.card:hover {
+  transform: scale(1.05);
 }
 
-strong {
-  font-size: 20px;
-  color: #34495e;
+.icon-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
 }
 
-a {
-  color: #2980b9;
-  text-decoration: none;
-  transition: color 0.3s ease;
+.icon-container img {
+  width: 40px;
+  height: 40px;
+  margin: 0 5px;
 }
 
-a:hover {
-  color: #3498db;
+.details {
+  padding: 20px;
+  margin: 10px;
+  background-color: #ecf0f1;
+  border-radius: 8px;
 }
 
 .about-img {
@@ -154,15 +239,16 @@ a:hover {
     font-size: 24px;
   }
 
-  p,
-  h3,
-  ul,
-  strong {
+  p {
     font-size: 16px;
   }
 
-  .about-img {
-    height: 80%;
+  .cards-container {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .card {
     width: 80%;
   }
 }
@@ -171,19 +257,12 @@ a:hover {
   .about {
     padding: 40px 100px;
   }
-  
-  .intro-section {
-    padding: 40px;
-  }
 
   h1 {
     font-size: 36px;
   }
 
-  p,
-  h3,
-  ul,
-  strong {
+  p {
     font-size: 18px;
   }
 }
