bfb28be0634d5b6b14a0d8910ca89c03e6e67b6b
Garabet Aslo <98517170+Gaasll@users.noreply.github.com>
2024-08-14 12:53:32 +0200

    Update AboutPage.vue

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -7,7 +7,7 @@
         spend my time staring at a computer screen.
       </p>
       <p>
-        In the <a href="/work" style="color: white;">projects section</a>, you will
+        In the <a style="color: white; font-weight: bold;">projects section</a>, you will
         find my
         experience in Computer Science
         - and it is forever growing! Below each
